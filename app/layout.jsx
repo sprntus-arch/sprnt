72a232ac910a23ac951c81b4266da84f7d51561f
sprntus-arch <sprntus@gmail.com>
2026-06-08 22:35:21 -0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,1 +1,7 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) { return ( {children} ) }
+export default function RootLayout({ children }) { 
+  return ( 
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
